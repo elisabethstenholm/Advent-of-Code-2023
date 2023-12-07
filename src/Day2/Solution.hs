1e8d1ed09837239a48f4f79a69f0ed207bce7fad
Elisabeth Bonnevier <elisabeth@stenholm.one>
2023-12-07 22:33:40 +0100

    wip - day 2

--- src/Day2/Solution.hs
+++ src/Day2/Solution.hs
@@ -2,8 +2,26 @@ module Day2.Solution where
 
 import Data.Void (Void)
 import Text.Megaparsec (Parsec)
+import Text.Megaparsec.Char (string, space, char)
+import Text.Megaparsec.Char.Lexer (decimal)
+import Data.Map (Map)
+import qualified Data.Map as Map
 
 type Parser = Parsec Void String
+
+numCubes :: (Num a) => Map String a
+numCubes = Map.fromList [("red", 12), ("green", 13), ("blue", 14)]
+
+pGameNumber :: (Num a) => Parser a
+pGameNumber = do
+    string "Game"
+    space
+    n <- decimal
+    char ':'
+    pure 0
+
+pGame :: (Num a) => Parser (a, [(a, String)])
+pGame = _
 
 solve1 :: String -> Integer
 solve1 _ = 0
