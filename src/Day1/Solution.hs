e2bc06ca421b527b25a218de914bde68fb92e1f2
Elisabeth Bonnevier <elisabeth@stenholm.one>
2023-12-07 11:18:39 +0100

    Solution to day 1

---
+++ src/Day1/Solution.hs
@@ -0,0 +1,58 @@
+module Day1.Solution where
+
+import Data.Char (isDigit)
+import Data.List (tails, inits)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
+import Control.Applicative
+
+wordToNum :: Map String Char
+wordToNum =
+  Map.fromList 
+    $ zip ["zero","one","two","three","four","five","six","seven","eight","nine"] ['0'..]
+
+splitAtDigits :: String -> [Either Char String]
+splitAtDigits [] = []
+splitAtDigits s@(x:xs)
+  | isDigit x = Left x : splitAtDigits xs
+  | otherwise =
+    let (s1, s2) = break isDigit s
+    in  Right s1 : splitAtDigits s2
+
+m2l :: (Alternative f) => Maybe a -> f a
+m2l = maybe empty pure
+
+-- Convert all numbers (words) in a string to digits
+allNumInStr :: String -> [Char]
+allNumInStr s = do
+  t <- tails s
+  i <- inits t
+  m2l $ Map.lookup i wordToNum
+
+-- Convert all numbers in a string (words or digits) to digits
+convertToNumbers :: String -> [Char]
+convertToNumbers s = do
+  d <- splitAtDigits s
+  either pure allNumInStr d
+
+firstAndLastToInteger :: String -> Integer
+firstAndLastToInteger = read . (\ s -> [head s, last s])
+
+-- Solution to part 1
+solve1 :: String -> Integer
+solve1 = sum . map (firstAndLastToInteger . filter isDigit) . lines
+
+-- Solution to part 2
+solve2 :: String -> Integer
+solve2 =
+  sum . map (firstAndLastToInteger . convertToNumbers) . lines
+
+foo :: IO ()
+foo = do
+  contents <- readFile "src/Day1/input.txt"
+  putStrLn "Part 1:"
+  print $ solve1 contents
+  putStrLn "Part 2:"
+  print $ solve2 contents
---
+++ src/Main.hs
@@ -0,0 +1,15 @@
+module Main where
+
+import Day1.Solution (solve1, solve2)
+
+main :: IO ()
+main = do
+  exData1 <- readFile "Day1/ex-input-1.txt"
+  exData2 <- readFile "Day1/ex-input-2.txt"
+  actualData <- readFile "Day1/input.txt"
+  putStrLn "Part 1:"
+  putStrLn $ "\tExample: " ++ show (solve1 exData1)
+  putStrLn $ "\tActual: " ++ show (solve1 actualData)
+  putStrLn "Part 2:"
+  putStrLn $ "\tExample: " ++ show (solve2 exData2)
+  putStrLn $ "\tActual: " ++ show (solve2 actualData)
